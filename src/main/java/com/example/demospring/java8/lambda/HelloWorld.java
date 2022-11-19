11198f28c60d0fc1ea9264b97090ab54a35a94d3
tung <tungreeboo@gmail.com>
2022-11-19 13:26:23 +0800

    update the java 8

---
+++ src/main/java/com/example/demospring/java8/lambda/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example.demospring.java8.lambda;
+
+import static java.lang.System.out;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        out.println("hello");
+    }
+}
